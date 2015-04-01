c760109d9c54961199a076b6b06880ae0a08ee73
MarcCloud <marccloud.11@gmail.com>
2015-04-01 11:46:17 -0600

    same as previous

--- client/login/login.jsx
+++ client/login/login.jsx
@@ -1,35 +1,26 @@
-var React=require('react'),
-    Form = require('../../partials/component-form.jsx'),
-    Input=require('../../partials/component-input.jsx');
-
+var React=require('react');
 var LoginForm = React.createClass({
-    changeUrl:function(err,res){
-        if(err!==null){
-            console.log(res);
-           // location.href='/dashboard';
-        }
+    submit:function(e){
+        e.preventDefault();
+        console.log('submited');
     },
     render:function(){
-        return(<Form url="/api/login" onSuccess={this.changeUrl} submitLabel="Log In">
-                    <div className="form-group">
-                        <label>Email</label>
-                        <Input  type="email"
-                                name="email"
-                                validations="isEmail"
-                                validationError="This is not a valid email"
-                                placeholder="email@example.com"/>
-                    </div>
-                    <div className="form-group">
-                        <label>Password</label>
-                        <Input
-                            type="password"
-                            name="password"
-                            validations="isPassword"
-                            validationError="This is not a valid password"
-                        />
-                    </div>
-                </Form>
-                )
+        return(
+                <div className="form col-md-3"  onSubmit={this.submit}>
+                    <form>
+                        <div className="form-group">
+                            <label htmlFor="user_mail">Email</label>
+                            <input id="user_mail" type="email" className="form-control" placeholder="mail@example.com"/>
+                        </div>
+                        <div className="form-group">
+                            <label htmlFor="user_password">Password</label>
+                            <input id="user_password" type="password" className="form-control" placeholder="Please enter your password"/>
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" className="btn btn-primary">Log In</button>
+                        </div>
+                    </form>
+                </div>)
     }
 });
 
